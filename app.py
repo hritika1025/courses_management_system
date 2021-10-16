0f238cdbd4e30cf8b4e112a5305734031ac28479
hritika1025 <hritika.cse29@gmail.com>
2021-10-16 14:03:30 +0530

    added backend for inserting department and faculty info

--- app.py
+++ app.py
@@ -30,6 +30,72 @@ def index():
         # return redirect(url_for('course'))
     return render_template('index.html',prof_name=prof_name,deptname=deptname) 
 
+@app.route("/admin_login", methods=['GET','POST'])
+def admin_login():
+    return render_template('admin_login.html')
 
+@app.route("/admin_dashboard")
+def admin_dashboard():
+    return render_template('admin_dashboard.html')
+
+@app.route("/department_info",methods=['GET','POST'])
+def department_info():
+    msg=''
+    if request.method == 'POST' and 'deptname' in request.form and 'cid' in request.form and 'cname' in request.form :
+        deptname = request.form['deptname']
+        cid = request.form['cid']
+        cname = request.form['cname']
+        if len(deptname) > 0 and len(cid) > 0 and len(cname) > 0:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM dept_cid_cname WHERE cid = % s AND deptname = %s', (cid,deptname, ) )
+            record1 = cursor.fetchone()
+            cursor.close()
+            if record1 :
+                msg='Record already exits!'
+            elif not re.match(r'[A-Z0-9]+', cid):
+                msg = 'Course Id must contain only Capital Letters and Numbers !'
+            else:
+                cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+                cursor.execute('INSERT INTO dept_cid_cname VALUES ( %s , %s , %s) ', (deptname,cid,cname, ) )
+                mysql.connection.commit()
+                cursor.close()
+                msg='Record Added Successfully!'
+        else:
+            msg='Please fill out the form!'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+
+    return render_template('department_info.html',msg=msg)
+
+@app.route("/prof_info",methods=['GET','POST'])
+def prof_info():
+    msg=''
+    if request.method == 'POST' and 'prof_name' in request.form and 'cid' in request.form  :
+        prof_name = request.form['prof_name']
+        cid = request.form['cid']
+        if len(prof_name) > 0 and len(cid) > 0 :
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM cid_pid_pname WHERE cid = % s AND prof_name = %s', (cid,prof_name, ) )
+            record1 = cursor.fetchone()
+            cursor.execute('SELECT * FROM dept_cid_cname WHERE cid = %s',(cid,))
+            record2=cursor.fetchone()
+            cursor.close()
+            if record1 :
+                msg='Record already exits!'
+            elif not record2:
+                msg=' This Course does not exist! Please add it first.'
+            elif not re.match(r'[A-Z0-9]+', cid):
+                msg = 'Course Id must contain only Capital Letters and Numbers !'
+            else:
+                cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+                cursor.execute('INSERT INTO cid_pid_pname VALUES (  NULL ,%s , %s) ', (cid,prof_name, ) )
+                mysql.connection.commit()
+                cursor.close()
+                msg='Record Added Successfully!'
+        else:
+            msg='Please fill out the form!'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('prof_info.html',msg=msg)
 
 app.run(debug=True)
--- course_mng.sql
+++ course_mng.sql
@@ -1,3 +1,4 @@
+
 CREATE DATABASE courses_management;
 USE courses_management;
 CREATE TABLE dept_cid_cname(
@@ -7,33 +8,40 @@ CREATE TABLE dept_cid_cname(
     PRIMARY KEY(cid)
 );
 CREATE TABLE cid_pid_pname(
+    prof_id INT NOT NULL AUTO_INCREMENT,
     cid VARCHAR(100) NOT NULL,
-    prof_id INT NOT NULL,
     prof_name VARCHAR(200) NOT NULL,
     PRIMARY KEY(prof_id),
     FOREIGN KEY(cid) REFERENCES dept_cid_cname(cid)
-);
+)AUTO_INCREMENT=1;
 CREATE TABLE pid_time(
+cid VARCHAR(100) NOT NULL,
     prof_id INT NOT NULL,
     time_slot VARCHAR(100) NOT NULL,
     room_num VARCHAR(50) NOT NULL,
     sem VARCHAR(50) NOT NULL,
     yr INT NOT NULL,
     PRIMARY KEY(time_slot, room_num, sem, yr),
-    FOREIGN KEY(prof_id) REFERENCES cid_pid_pname(prof_id)
+    FOREIGN KEY(prof_id) REFERENCES cid_pid_pname(prof_id),
+     FOREIGN KEY(cid) REFERENCES dept_cid_cname(cid)
 );
+CREATE TABLE admin( admin_id VARCHAR(100) NOT NULL,
+                     password VARCHAR(100) NOT NULL);
 
-INSERT INTO dept_cid_cname(deptname, cid, cname) VALUES('CSE', 'CS-207', 'Database Management System');
-INSERT INTO dept_cid_cname(deptname, cid, cname) VALUES('CSE', 'CS-203', 'Data Structures And Algorithm');
-INSERT INTO dept_cid_cname(deptname, cid, cname) VALUES('CSE', 'CS-201', 'Discrete Maths');
-INSERT INTO dept_cid_cname(deptname, cid, cname) VALUES('EE', 'EE-203', 'Electronic Devices');
-INSERT INTO dept_cid_cname(deptname, cid, cname) VALUES('EE', 'EE-205', 'Network Theory');
-INSERT INTO dept_cid_cname(deptname, cid, cname) VALUES('EE', 'EE-104', 'Basic Electrical and Electronics Engineering');
+INSERT INTO dept_cid_cname(deptname, cid, cname) VALUES('CSE', 'CS207', 'Database Management System');
+INSERT INTO dept_cid_cname(deptname, cid, cname) VALUES('CSE', 'CS203', 'Data Structures And Algorithm');
+INSERT INTO dept_cid_cname(deptname, cid, cname) VALUES('CSE', 'CS201', 'Discrete Maths');
+INSERT INTO dept_cid_cname(deptname, cid, cname) VALUES('EE', 'EE203', 'Electronic Devices');
+INSERT INTO dept_cid_cname(deptname, cid, cname) VALUES('EE', 'EE205', 'Network Theory');
+INSERT INTO dept_cid_cname(deptname, cid, cname) VALUES('EE', 'EE104', 'Basic Electrical and Electronics Engineering');
 
-INSERT INTO cid_pid_pname(cid, prof_id, prof_name) VALUES('CS-207', 1, 'Nagendra Kumar');
-INSERT INTO cid_pid_pname(cid, prof_id, prof_name) VALUES('EE-203', 2, 'Abhinav Kranti');
-INSERT INTO cid_pid_pname(cid, prof_id, prof_name) VALUES('EE-104', 3, 'Abhinav Kranti');
-INSERT INTO pid_time(prof_id, time_slot, room_num, sem, yr) VALUES(1, 'Monday 11:00-11:50', 'L-02', 'AUTUMN', 2021);
+INSERT INTO cid_pid_pname( prof_id, cid, prof_name) VALUES( 1,'CS207', 'Nagendra Kumar');
+INSERT INTO cid_pid_pname( prof_id, cid, prof_name) VALUES( 2,'EE203', 'Abhinav Kranti');
+INSERT INTO cid_pid_pname( prof_id, cid, prof_name) VALUES( 3,'EE104', 'Abhinav Kranti');
+INSERT INTO pid_time(cid,prof_id, time_slot, room_num, sem, yr) VALUES('CS207',1, 'Monday 11:00-11:50', 'L-02', 'AUTUMN', 2021);
+
+INSERT INTO admin(admin_id,password) VALUES('cse200001029@iiti.ac.in','Hritika123');
+SELECT * FROM admin;
 SELECT * FROM cid_pid_pname;
 SELECT * FROM pid_time;
 SELECT cname FROM dept_cid_cname WHERE deptname = 'EE' and cid in (SELECT cid FROM cid_pid_pname WHERE prof_id = (SELECT prof_id FROM pid_time WHERE sem = 'AUTUMN' AND yr = 2021));
